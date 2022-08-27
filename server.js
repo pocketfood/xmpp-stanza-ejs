d8d1ca93d7d8523a04a843e18c9765f01ded0bf2
pocketfood <johnfrayster@gmail.com>
2022-08-26 22:36:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const express = require("express")
+const router = require("./routes/xmppconnection")
+const app = express()
+const port = process.env.port || 8083;
 
+// URL encoded bodies
+app.use(express.urlencoded({extended: false}));
+
+// Root url
+app.use('/', router)
+
+// ejs
+app.set("view engine", "ejs")
+
+console.log('running on',port,'\n');
+app.listen(port)
